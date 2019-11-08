641a0780b5039314cc7436e2fd7490a3186250da
lazarspasic <44000018+Laks96@users.noreply.github.com>
2019-11-08 14:23:22 +0100

    event update

---
+++ events/zad1/doc.js
@@ -0,0 +1,23 @@
+function color() {
+    var y = document.querySelector('div')
+    y.classList.toggle('first-div')
+
+
+}
+
+function color2() {
+    var secondInput = document.querySelector('#input2');
+    secondInput.removeAttribute('onclick');
+}
+
+
+/*Advance
+Adapt basic example
+On button click, automatically toggle background
+Have a button to cancel automatic toggle
+*/
+
+
+
+
+
---
+++ events/zad2/main.js
@@ -0,0 +1,9 @@
+function message() {
+    var a = document.querySelector('#input1');
+    var b = document.createElement('span');
+    b.textContent = a.value;
+
+}
+
+
+
