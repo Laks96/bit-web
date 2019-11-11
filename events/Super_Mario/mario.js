85e19bccf7bd35c8ea31b3f4bef002e7f81c9b75
lazarspasic <44000018+Laks96@users.noreply.github.com>
2019-11-11 18:12:04 +0100

    Mario

---
+++ events/Super_Mario/mario.js
@@ -0,0 +1,17 @@
+var marioStay = document.querySelector('.marioStop');
+
+
+
+if ()
+
+
+    marioStay.addEventListener('keydown', function (e) {
+
+        marioStay.classList.toggle("marioRun")
+
+
+
+
+    })
+
+
