1c478acde1436136cdb93391984697f5c5d8277f
BIT Student <classroom@bgit.rs>
2019-11-20 18:08:36 +0100

    update

--- BitShow/js/control.js
+++ BitShow/js/control.js
@@ -1,23 +1,29 @@
 const control = (function (ui, data) {
+    function onShowClick() {
+        const showId = $(this).attr('data-show-id');
 
-    $(function () {
-        // Handler for .ready() called.
-
-    //pozivanje f-je iz ui sa nizom is data
-
-        function success(shows) {
-            ui.display(shows)
-
+        function onSuccessShow(show) {
+            ui.renderShow(show);
         }
+
+        data.fetchShow(showId, onSuccessShow);
+    }
+
+    function init() {
+        function success(shows) {
+            ui.display(shows);
+            ui.getShowCards().on('click', onShowClick)
+        }
+
         data.fetchShows(success);
+    }
 
-
-     
-    });
-
-
-
-
-  
-
+    return {
+        init
+    }
 })(ui, data);
+
+
+$(function () {
+    control.init();
+});
--- BitShow/js/ui.js
+++ BitShow/js/ui.js
@@ -1,33 +1,88 @@
 const ui = (function () {
+    const $container = $('.container');
+
+    function getShowCards() {
+        return $('.show-card');
+    }
 
 
     function display(shows) {
+        clear();
+
+        const $header = $('<h1>')
+            .text('Popular Shows')
+            .addClass('header');
+        $container.append($header);
+
+        const $grid = $('<div>')
+            .addClass('flex-container');
 
         shows.forEach(show => {
-            // const li = $(`<li>${user.name} </li>`);
-            // $('.listOfUser').append(li);
+            const $card = $(`<div>`)
+                .attr('data-show-id', show.id)
+                .addClass('show-card')
+                .addClass('style-div');
 
-            const div = $(`<div>`);
-            const span = $(`<span>${show.name}</span>`);
+            const $img = $('<img>')
+                .attr('src', show.image)
+                .addClass('show-image')
+            $card.append($img);
 
-            const img = $('<img>').attr('src', show.image).addClass('show-image')
-            const imgClickable = $('<a>').attr('href', show.url);
+            const $title = $(`<p>`)
+                .text(show.name)
+                .addClass('p-style-yu');
+            $card.append($title);
 
-            imgClickable.append(img);
-
-            div.append(imgClickable);
-            div.append(span);
-            const mainWrap = $('.main-wrap')
-            mainWrap.append(div);
+            $grid.append($card);
         });
+
+        $container.append($grid);
+    }
+
+    /*WE DELETE DOM*/
+
+    function clear() {
+        $container.html('');
+    }
+
+    function renderShow(show) {
+        clear();
+
+
+        /*NEW SINGLE SHOW*/
+
+
+        const $singleImage = $('<img>')
+            .attr('src', show.image)
+            .addClass('show-single-image')
+
+        $container.append($singleImage);
+
+        const $seasonInfo = $('<p>')
+            .text("texxxx", show.premierDate)
+
+
+            .addClass("season-text");
+
+        console.log(show.premierDate);
+
+        $container.append($seasonInfo);
+
+
+
+
+
+
+
+
+
+
 
     }
 
     return {
-        display
+        display,
+        renderShow,
+        getShowCards
     }
-
-
-
-
 })();
